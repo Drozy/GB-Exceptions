303a5fa39e8f37c7e48c53de652ddfefcdb3f8b7
Yakovlev Andrew <drozy@yandex.ru>
2023-08-12 00:28:18 +0300

    Добавлено домашнее задание

--- lesson3/HomeWork.java
+++ lesson3/HomeWork.java
@@ -1,5 +1,12 @@
 package lesson3;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class HomeWork {
     /*
     Напишите приложение, которое будет запрашивать у пользователя следующие данные в произвольном порядке,
@@ -23,4 +30,88 @@ public class HomeWork {
     В него в одну строку должны записаться полученные данные, вида:
     Фамилия Имя Отчество дата_рождения номер_телефона пол
      */
+
+    public static class UserInfoApp {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите данные (в формате: фамилия имя отчество дата_рождения номер_телефона пол):");
+            String input = scanner.nextLine();
+            try {
+                String[] data = checkData(input);
+
+                String lastName = checkName(data[0]);
+                String firstName = checkName(data[1]);
+                String middleName = checkName(data[2]);
+                Date dateOfBirth = parseDate(data[3]);
+                String phoneNumber = checkPhone(data[4]);
+                char gender = checkGender(data[5]);
+
+                String result = lastName + " " +
+                        firstName + " " +
+                        middleName + ", " +
+                        formatDate(dateOfBirth) + ", " +
+                        phoneNumber + ", " +
+                        gender;
+                saveInFile(lastName, result);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка: Неверный формат номера телефона");
+            } catch (ParseException e) {
+                System.out.println("Ошибка: Неверный формат даты");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ошибка: " + e.getMessage());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private static Date parseDate(String dateStr) throws ParseException {
+            SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+            return format.parse(dateStr);
+        }
+
+        private static String formatDate(Date date) {
+            SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+            return format.format(date);
+        }
+
+        private static String[] checkData(String input) {
+            String[] data = input.split(" ");
+            if (data.length > 6) {
+                throw new IllegalArgumentException("Количество данных больше требуемого");
+            } else if (data.length < 6) {
+                throw new IllegalArgumentException("Количество данных меньше требуемого");
+            }
+            return data;
+        }
+
+        private static String checkPhone(String data) {
+            if (data.matches("\\d+"))
+                return data;
+            else throw new NumberFormatException();
+        }
+
+        private static char checkGender(String data) {
+            if (data.equals("f") || data.equals("m"))
+                return data.charAt(0);
+            else throw new IllegalArgumentException("Неверный формат пола");
+        }
+
+        private static String checkName(String data) {
+            if (data.matches("^[а-яА-Я]*$") || data.matches("^[a-zA-Z]*$"))
+                return data;
+            else throw new IllegalArgumentException("ФИО содержит не только буквы");
+        }
+        private static void saveInFile(String filePath, String data) throws WriteInFileException {
+            try (FileWriter writer = new FileWriter(filePath, true)){
+                writer.write(data);
+                writer.write("\n");
+                writer.flush();
+            } catch (IOException e){
+                throw new WriteInFileException("Ошибка записи в файл ", e);
+            }
+        }
+    }
 }
+
+
